46bd6a52a72e7557f1f178e24cbfb6c5c2a5df06
Simón Jiménez Tamayo <65623835+sijita@users.noreply.github.com>
2024-12-16 15:14:54 -0500

    feat: add initial config and necessary packages

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,14 @@
-import type { Config } from "tailwindcss";
+import { nextui } from '@nextui-org/react';
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
-      },
-    },
-  },
-  plugins: [],
+  theme: {},
+  darkMode: 'class',
+  plugins: [nextui()],
 } satisfies Config;
