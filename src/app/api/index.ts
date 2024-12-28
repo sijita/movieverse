31077767231b066dc3ad5cd31ff44e9cc27ed43c
Simón Jiménez Tamayo <u20191176294@usco.edu.co>
2024-12-28 14:12:44 -0500

    fix: add error handling in api fetchs
    - Add new api fetchs

--- src/app/api/index.ts
+++ src/app/api/index.ts
@@ -1,13 +1,63 @@
 import axios from '@/utils/lib/axios-client';
 
 export const fetchRandomMovie = async () => {
-  const fetchRandomMovie = await axios.get(
-    '/discover/movie?language=es-ES&page=1&year=2024'
-  );
+  try {
+    const { data } = await axios.get(
+      '/discover/movie?language=es-ES&page=1&year=2024'
+    );
 
-  const randomMovie = fetchRandomMovie.data.results;
-  const randomIndex = Math.floor(Math.random() * randomMovie.length);
-  const movie = randomMovie[randomIndex];
+    const randomMovie = data.results;
+    const randomIndex = Math.floor(Math.random() * randomMovie.length);
+    const movie = randomMovie[randomIndex];
 
-  return movie;
+    return movie;
+  } catch {
+    return [];
+  }
+};
+
+export const fetchMoviesCategories = async () => {
+  try {
+    const { data } = await axios.get('/genre/movie/list?language=es-ES');
+    const categories = data.genres;
+
+    return categories;
+  } catch {
+    return [];
+  }
+};
+
+export const fetchTopRatedMovies = async () => {
+  try {
+    const { data } = await axios.get('/movie/top_rated?language=es-ES&page=1');
+    const topRatedMovies = data.results;
+
+    return topRatedMovies;
+  } catch {
+    return [];
+  }
+};
+
+export const fetchPopularMovies = async () => {
+  try {
+    const { data } = await axios.get('/movie/popular?language=es-ES&page=1');
+    const popularMovies = data.results;
+
+    return popularMovies;
+  } catch {
+    return [];
+  }
+};
+
+export const fetchMoviesByCategory = async (id: string) => {
+  try {
+    const { data } = await axios.get(
+      `/discover/movie?with_genres=${id}&language=es-ES`
+    );
+    const movies = data.results;
+
+    return movies;
+  } catch {
+    return [];
+  }
 };
